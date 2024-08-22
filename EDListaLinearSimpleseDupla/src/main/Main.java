3f7b60aff1b0a8b4c0604209636b6c8acf1df36a
AngeloModanez <angelomodanez30@gmail.com>
2024-08-22 16:20:37 -0300

    :sparkles: feat: Association signals

--- EDListaLinearSimpleseDupla/src/main/Main.java
+++ EDListaLinearSimpleseDupla/src/main/Main.java
@@ -11,67 +11,40 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        EDStack stackSquareBrackets = new EDStack(); // []
-        EDStack stackParentheses = new EDStack(); // ()
-        EDStack stackCurlyBrackets = new EDStack(); // {}
-
         Scanner in = new Scanner(System.in);
 
         System.out.println("\nInforme a expressão: ");
         String sExp = in.next();
-        boolean error = false;
+        in.close();
 
-        for (int i = 0; i < sExp.length(); i++) {
-            switch (sExp.charAt(i)) {
-                case '[': {
-                    stackSquareBrackets.push(new Node("1", 1));
-                    break;
-                }
-                case ']': {
-                    if (stackSquareBrackets.empty()) {
-                        error = true;
-                    } else {
-                        stackSquareBrackets.pop();
-                    }
-                    break;
-                }
-                case '(': {
-                    stackParentheses.push(new Node("1", 1));
-                    break;
-                }
-                case ')': {
-                    if (stackParentheses.empty()) {
-                        error = true;
-                    } else {
-                        stackParentheses.pop();
-                    }
-                    break;
-                }
-                case '{': {
-                    stackCurlyBrackets.push(new Node("1", 1));
-                    break;
-                }
-                case '}': {
-                    if (stackCurlyBrackets.empty()) {
-                        error = true;
-                    } else {
-                        stackCurlyBrackets.pop();
-                    }
-                    break;
-                }
-            }
-            if (error) {
-                break;
-            }
-        }
+        new BalancedExpression();
+        boolean result = BalancedExpression.verify(sExp);
 
-        if (error
-                || !stackSquareBrackets.empty()
-                || !stackParentheses.empty()
-                || !stackCurlyBrackets.empty()) {
-            System.out.println("\nExpressao incorreta");
+        if (result) {
+            System.out.println("\nExpressao correta");
         } else {
-            System.out.println("\nExpressão Ok");
+            System.out.println("\nExpressão incorreta");
         }
+    }
+}
+
+class BalancedExpression {
+
+    public static boolean verify(String expressao) {
+        EDStack pilha = new EDStack();
+
+        for (char c : expressao.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                pilha.push(new Node(c + "", (int) c));
+            } else if (c == ')' && (pilha.empty() || (char) ((int) pilha.pop().getValue()) != '(')) {
+                return false;
+            } else if (c == '}' && (pilha.empty() || (char) ((int) pilha.pop().getValue()) != '{')) {
+                return false;
+            } else if (c == ']' && (pilha.empty() || (char) ((int) pilha.pop().getValue()) != '[')) {
+                return false;
+            }
+        }
+
+        return pilha.empty();
     }
 }
